6e9b63a80e8b4d219dd021e4d8d4f9b08f5b38c6
mooncores <84422850+mooncores@users.noreply.github.com>
2021-06-03 14:13:30 -0400

    Add files via upload

---
+++ status.lua
@@ -0,0 +1,11 @@
+return {
+    ["🟡 A Bizarre Journey"] = 5233268561,
+    ["🟡 Arsenal"] = 286090429,
+    ["🟡 Anime Battle Arena"] = 4520749081,
+    ["🟡 Bad Business"] = 3233893879,
+    ["🟡 K.A.T"] = 621129760,
+    ["🟡 King Legacy"] = 4520749081,
+    ["🟡 Phantom Forces"] = 292439477,
+    ["🟡 Vehicle Simulator"] = 171391948,
+    ["🟡 Wisteria"] = 4723618670
+}
