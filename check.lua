66633bfcecc3b81a68cff41bd2c6d9a9fa4750a3
mooncores <84422850+mooncores@users.noreply.github.com>
2021-09-08 02:50:14 -0400

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,11 +1,10 @@
 return {
     ["🟢 A Universal Time"] = 5130598377,
     ["🟡 Arsenal"] = 286090429,
-    ["🟡 Anime Battle Arena"] = 4520749081,
     ["🟡 Bad Business"] = 3233893879,
     ["🟡 K.A.T"] = 621129760,
     ["🟡 King Legacy"] = 4520749081,
     ["🟡 Phantom Forces"] = 292439477,
     ["🟡 Vehicle Simulator"] = 171391948,
-    ["🟡 Wisteria"] = 4723618670
+    ["🟡 Demonfall"] = 4723618670
 }
